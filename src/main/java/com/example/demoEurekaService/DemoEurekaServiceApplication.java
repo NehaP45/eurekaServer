873704fcd7273979ae6039a6138e0b7761f48d51
Purohit <neha.purohit3@dxc.com>
2022-10-12 16:47:40 +0530

    Eureka server code commit

---
+++ src/main/java/com/example/demoEurekaService/DemoEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoEurekaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaServiceApplication.class, args);
+	}
+
+}
